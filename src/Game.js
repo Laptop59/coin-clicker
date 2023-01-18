bbc8001c82c9243cbdefe1de3b5afbda7ef0a1a4
Laptop59 <avassyusuvarna@gmail.com>
2023-01-18 12:27:05 +0530

    comma system implemented

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,11 @@
 import COIN_SRC from "./images/big_coin.png";
 import BIG_COIN_SRC from "./images/coin_button.png";
+import BLANK_SRC from "./images/blank_icon.png";
 
 class Game {
     coins = 0
-    cpc = 1
+    coinsPerClick = 1
+    coinsPerSec = 0
 
     ready = 0;
     canvas = null;
@@ -21,6 +23,9 @@ class Game {
     loadImages() {
         this.images.big_coin = new Image();
         this.images.big_coin.src = BIG_COIN_SRC;
+
+        document.getElementsByClassName("coins-header")[0].getElementsByTagName("div")[0].appendChild(this.makeIcon(0, 0, 32));
+        document.getElementsByClassName("cpc-header")[0].getElementsByTagName("div")[0].appendChild(this.makeIcon(0, 0, 24));
     }
 
     start() {
@@ -42,7 +47,8 @@ class Game {
     }
 
     updateText() {
-        document.getElementsByTagName("h1").innerHTML = '<img src="">' + this.coinSize;
+        document.getElementsByClassName("coins-header")[0].getElementsByTagName("span")[0].innerHTML = "" + this.commify(this.coins, true);
+        document.getElementsByClassName("cpc-header")[0].getElementsByTagName("span")[0].innerHTML = this.coinsPerSec ? (this.commify(this.coinsPerSec) + "/second") : (this.commify(this.coinsPerClick) + "/click");
     }
 
     resizeCanvas() {
@@ -81,7 +87,42 @@ class Game {
 
     coinClick() {
         this.coinSize = 0.75 * 1.5;
-        this.coins += this.cpc;
+        this.coins += this.coinsPerClick * this.coins + 1;
+    }
+
+    makeIcon(x, y, size = 128) {
+        // Each icon is 128x128.
+        const img = new Image();
+        img.src = BLANK_SRC;
+        img.className = "icon";
+        img.style = `background-position: ${-x}px ${-y}px; width: ${size}px; height: ${size}px; background-size: ${size/128 * 2048}px`;
+        return img;
+    }
+
+    commify(number, br) {
+        number = Math.floor(number);
+
+        if (!isFinite(number)) return number + "";
+        if (number < 0) return "-" + this.this.commify(-number);
+        if (number < 1000000000) return number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+
+        const illion = Math.floor(Math.log10(number) / 3);
+        const starting = Math.pow(10, illion * 3);
+        
+        const float = number / starting;
+
+        return float.toFixed(3) + (br ? "<br><span>" : " ") + this.illionSuffix(illion - 1) + (br ? "</span>" : "");
+    }
+
+    illionSuffix(illion) {
+        if (illion == 100) return "centillion";
+        if (illion == 101) return "uncentillion";
+        if (illion < 10) {
+            return ["million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion"][illion - 1];
+        }
+        let unit = ["", "un", "duo", "tre", "quattuor", "quin", "sex", "septen", "octo", "novem"][illion % 10];
+        let ten = ["", "dec", "vigint", "trigint", "quadragint", "quinquagint", "sexagint", "septuagint", "octoginta", "nonaginta"][Math.floor(illion / 10) % 10];
+        return unit + ten + "illion";
     }
 }
 
